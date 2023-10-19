7e5373e7a6e0709d4c6ac2801bafc6aa3716522f
avkkiran-sails <avk.kirankumar@sailssoftware.com>
2023-10-19 13:43:32 +0530

    adding files required for initial setup
    adding files required for initial setup

---
+++ src/test/java/com/sails/software/interview/builder/InterviewBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sails.software.interview.builder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
