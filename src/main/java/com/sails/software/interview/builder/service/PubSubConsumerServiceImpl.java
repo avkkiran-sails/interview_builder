36d91390798b48f2c655c59e0ccc021fd5208ca7
Phani Bnr <phanisai@sails.solutions>
2023-10-31 14:47:20 +0530

    pubsub consumer

--- src/main/java/com/sails/software/interview/builder/service/PubSubConsumerServiceImpl.java
+++ src/main/java/com/sails/software/interview/builder/service/PubSubConsumerServiceImpl.java
@@ -1,35 +1,108 @@
 package com.sails.software.interview.builder.service;
 
+import com.google.api.gax.rpc.DeadlineExceededException;
 import com.google.cloud.spring.pubsub.core.PubSubTemplate;
+import com.google.cloud.spring.pubsub.support.AcknowledgeablePubsubMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
+import reactor.core.Disposable;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+import reactor.core.publisher.Mono;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 @RequiredArgsConstructor
 @Service
 public class PubSubConsumerServiceImpl {
 
-    private final PubSubTemplate publisher;
+    private final PubSubTemplate pubSubTemplate;
+
+    private Disposable subscription;
 
     @Value("${pubsub.subscription}")
-    private String subscription;
+    private String subscriptionTopic;
 
     public void consumeMessage(){
         try {
             System.out.println("Inside consumeMessage");
-            while (true) {
-                var messages = publisher.pullAsync(subscription, 100, true);
-                for (var message : messages.get()) {
-                    System.out.println(message.getPubsubMessage().getData());
-                    message.ack();
-                }
-            }
+            var flux = this.poll(subscriptionTopic);
+            subscription = this.processing(flux.limitRate(1)).subscribe();
+
             } catch(Exception ex){
                 System.out.println(ex.getMessage());
             }
     }
+
+    public Flux<AcknowledgeablePubsubMessage> poll(String subscriptionName) {
+        return Flux.create(
+                sink ->
+                        sink.onRequest(
+                                count -> {
+                                    try {
+                                        pull(subscriptionName, count, sink);
+                                    } catch (ExecutionException e) {
+                                        e.printStackTrace();
+                                    } catch (InterruptedException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                        )
+        );
+    }
+
+    private void pull(String subscriptionName, long numRequested,
+                      FluxSink<AcknowledgeablePubsubMessage> sink) throws ExecutionException, InterruptedException {
+        int intDemand = numRequested > Integer.MAX_VALUE ? Integer.MAX_VALUE : (int) numRequested;
+
+        try {
+            var msgs = this.pubSubTemplate.pullAsync(subscriptionName, intDemand, false);
+
+            msgs.whenComplete(
+                    (messages, exception) -> {
+                        if (!sink.isCancelled()) {
+                            messages.forEach(sink::next);
+                        }
+                        if (!sink.isCancelled()) {
+                            long numToPull = numRequested - messages.size();
+                            if (numToPull > 0) {
+                                try {
+                                    pull(subscriptionName, numToPull, sink);
+                                } catch (ExecutionException e) {
+                                    e.printStackTrace();
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    });
+
+            System.out.println(msgs.get().size());
+        }catch (Exception ex){
+            System.out.println(ex);
+        }
+    }
+
+    private final Flux<?> processing(Flux<AcknowledgeablePubsubMessage> flux){
+        return flux.flatMap(this::processing);
+    }
+
+    private Mono<?> processing(AcknowledgeablePubsubMessage message){
+        return Mono
+                .just(message)
+                .flatMap(this::processMessage);
+    }
+
+    private Mono<?> processMessage(AcknowledgeablePubsubMessage message){
+        System.out.println(message.getPubsubMessage().getData());
+        message.ack();
+        return Mono.just(message);
+    }
+
 
     @EventListener(ApplicationStartedEvent.class)
     public void startConsumer(){
